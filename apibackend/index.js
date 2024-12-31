5f5e55494fa20196e54da409749a032e2a06cd4c
Muzamil-26 <155745476+Muzamil-26@users.noreply.github.com>
2024-12-31 12:22:00 +0530

    Update index.js

--- apibackend/index.js
+++ apibackend/index.js
@@ -8,11 +8,11 @@ import cors from 'cors';
 import cookieparser from 'cookie-parser';
 
 
- const corsOptions = {
-    origin: 'https://progidyapi.vercel.app/', // specify your frontend URL
-    credentials:true
-};
-app.use(cors(corsOptions));
+ //const corsOptions = {
+    //origin: 'https://progidyapi.vercel.app/', // specify your frontend URL
+   // credentials:true
+//};
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieparser());
